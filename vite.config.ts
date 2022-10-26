d8bf66578351a83ed0fff7f74ae1cb301cb0fd91
FanmKaribeyenn <vnovembre971@gmail.com>
2022-10-26 18:29:05 -0400

    add serice worker

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html}', '**/*.{svg,png,jpg,gif}'],
+        // maximumFileSizeToCacheInBytes:17 * 1024 * 1024
       },
     }),
   ],
