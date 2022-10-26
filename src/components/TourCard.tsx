a354ac4262076df3b52daaa88ce91bee335ad9e6
FanmKaribeyenn <vnovembre971@gmail.com>
2022-10-26 00:18:53 -0400

    add login  / register ans home section

---
+++ src/components/TourCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { MdLocationOn } from 'react-icons/md';
+import LazyLoad from 'react-lazy-load';
+import { Link } from 'react-router-dom';
+
+import Rating from '@mui/material/Rating';
+
+import './styles/tourCard.scss';
+
+export default function TourCard({ post, handleViewTour }: any) {
+  return (
+    <div className="tour-card-content" onClick={() => handleViewTour(post.id)}>
+      <LazyLoad>
+        <img alt="About img" src={`${post.cover}`} />
+      </LazyLoad>
+      <div className="tour-card-desc">
+        <p>Span, discover</p>
+        <div className="card-star">
+          <MdLocationOn />
+          {post.city}
+        </div>
+        <div className="star">
+          <Rating name="read-only" value={post.stars} readOnly />({post.stars})
+        </div>
+        <p className="desc">{post.desc}</p>
+        <li>
+          <Link className="ctn" to="/register">
+            Read More
+          </Link>
+        </li>
+      </div>
+    </div>
+  );
+}
